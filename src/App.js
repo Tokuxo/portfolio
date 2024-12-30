79be6819028520aa1de389394fd745e9cac14ada
Tokuxo <tokuxocommercial@gmail.com>
2024-12-30 10:44:30 +0800

    Page Changing

--- src/App.js
+++ src/App.js
@@ -15,12 +15,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
+        <div className='topMenu'>
           <ul>
             <li><button type='button' id={elementId[0]} onClick={() => changeId(0)}>Info</button></li>
             <li><button type='button' id={elementId[1]} onClick={() => changeId(1)}>Past Work</button></li>
             <li><button type='button' id={elementId[2]} onClick={() => changeId(2)}>Contact</button></li>
           </ul>
+        </div>
+        <div className={elementId[0]}>
+          <p>Introduction to Modeler Tokuxo</p>
+          <img src="Fumo.jpg" alt="wheres my fucking fumo"></img>
+        </div>
+        <div className={elementId[1]}>
+          <h1>1</h1>
+        </div>
+        <div className={elementId[2]}>
+          <h1>2</h1>
         </div>
       </header>
     </div>
